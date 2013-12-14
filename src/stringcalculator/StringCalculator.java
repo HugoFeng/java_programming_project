3cadf464f268ffcd34c33ff1a97d0fab6d0c8ec1
Hugo.fxy <hugo.fxy@gmail.com>
2013-12-14 21:34:57 +0100

    Now a single value input can equal to itself. All comments added.

--- src/stringcalculator/StringCalculator.java
+++ src/stringcalculator/StringCalculator.java
@@ -9,8 +9,9 @@ import java.util.Stack;
  * @version 1.0
  * StringCalculator is a class offers expression tokenization and calculation. 
  * The following operators are supported:
- * 		Basic: +, -, *, /, (, )
- * 		Scientific: ^, E
+ * 		Basic: +, -, *, /
+ * 		Scientific: ^, E, %
+ * 		Syntax: (, )
  *
  */
 public class StringCalculator {
@@ -29,16 +30,19 @@ public class StringCalculator {
 
 	}
 
-	// The super class of 
+	// The super class of all operators
+	// easier to manage or add new operators
 	protected abstract class Operator {
-		public final int level;
+		public final int level;		// Define the priority of an operator
 
 		protected Operator(int l) {
 			level = l;
 		}
 
+		// All operators have the run method
 		public abstract Double run(Double val1, Double val2);
 
+		// Recognize each operator by the toString() methond
 		public abstract String toString();
 	}
 
@@ -152,11 +156,13 @@ public class StringCalculator {
 	Stack<Double> valueStack;
 	Stack<String> opStrStack;
 
+	// Constructor, initialize stacks to store history states
 	public StringCalculator() {
 		valueStack = new Stack<Double>();
 		opStrStack = new Stack<String>();
 	}
 
+	// Simple factory method design pattern, return an Operator instance according to the input string
 	protected Operator operatorFactory(String s) {
 		if (s.equals("+"))
 			return new OperatorAdd();
@@ -175,15 +181,16 @@ public class StringCalculator {
 		else
 			return null;
 	}
-
+	
+	// Input a value or operator, return a value if any operation is executed, else return empty string
 	public String input(String s) throws ExpressionSyntaxError,
 			ArithmeticException {
 		Double valueDouble = Double.NaN;
-		if (isNumeric(s))
+		if (isNumeric(s))		// Input is a number, push into value stack
 			valueStack.push(Double.parseDouble(s));
 		else if (s.equals("("))
 			opStrStack.push(s);
-		else if (s.equals(")")) {
+		else if (s.equals(")")) {		// Input is a right bracket, finish what is inside
 			if (opStrStack.empty())
 				throw new ExpressionSyntaxError();
 			while (!opStrStack.peek().equals("(")) {
@@ -196,24 +203,28 @@ public class StringCalculator {
 				valueStack.push(valueDouble);
 			}
 			opStrStack.pop(); // Pop out "(" in opStrStack
-		} else if (s.equals("=")) {
-			if (valueStack.size() < 2)
+		} else if (s.equals("=")) {		// finish all the calculation
+			if (valueStack.size() == 1 && opStrStack.empty()) {		// Only one number, no operation called, return itself
+				valueDouble = valueStack.pop();
+			} else if (valueStack.size() < 2)		// Not sufficient arguments
 				throw new ExpressionSyntaxError();
-			while (!opStrStack.empty()) {
-				Operator operator = operatorFactory(opStrStack.pop());
-				if (valueStack.size() < 2)
-					throw new ExpressionSyntaxError();
-				Double val2 = valueStack.pop();
-				Double val1 = valueStack.pop();
-				valueDouble = operator.run(val1, val2);
-				valueStack.push(valueDouble);
+			else {		// Finish the calculation
+				while (!opStrStack.empty()) {
+					Operator operator = operatorFactory(opStrStack.pop());
+					if (valueStack.size() < 2)
+						throw new ExpressionSyntaxError();		// Not sufficient arguments
+					Double val2 = valueStack.pop();
+					Double val1 = valueStack.pop();
+					valueDouble = operator.run(val1, val2);
+					valueStack.push(valueDouble);
+				}
 			}
-		} else {
+		} else {		// Input is an operator
 			Operator thisOperator = operatorFactory(s);
-			if(thisOperator == null) throw new ExpressionSyntaxError();
-			while (!opStrStack.empty()
-					&& !opStrStack.peek().equals("(")
-					&& operatorFactory(opStrStack.peek()).level >= thisOperator.level) {
+			if(thisOperator == null) throw new ExpressionSyntaxError();		// Illegal operator detected
+			while (!opStrStack.empty()		// Finish some of the operations stored in the operation stack with the same or higher priorities
+					&& !opStrStack.peek().equals("(")	// rest of the operations are outside the bracket which means lower priority
+					&& operatorFactory(opStrStack.peek()).level >= thisOperator.level) {	// Continue calculating if the rest have the same or higher priority
 				Operator preOperator = operatorFactory(opStrStack.pop());
 				Double val2 = valueStack.pop();
 				Double val1 = valueStack.pop();
@@ -224,13 +235,15 @@ public class StringCalculator {
 		}
 
 		if (valueDouble.isNaN()) {
-			return "";
+			return "";		// No operation is executed
 		} else {
-			return String.valueOf(valueDouble);
+			return String.valueOf(valueDouble);		// Return operation result
 		}
 
 	}
 
+	//	Static method, compute a line of expressions all at once
+	//  This line must be finished with "="
 	public static String getResultFromExpression(String expression)
 			throws ArithmeticException, ExpressionSyntaxError {
 		StringCalculator expreessionCalculator = new StringCalculator();
@@ -243,11 +256,13 @@ public class StringCalculator {
 		return output;
 	}
 
+	// Static method
+	// Parse a line of expressions into arguments and operators in a queue
 	protected static LinkedList<String> Tokenize(String expreString) {
 		Queue<String> tokenized = new LinkedList<String>();
 		int start = 0;
-		boolean previousArgumentIsNullOrLeftBracket = true;
-		for (int i = 0; i < expreString.length(); i++) {
+		boolean previousArgumentIsNullOrLeftBracket = true;		//  Distinguish "-" if it's a negative sign in value 
+		for (int i = 0; i < expreString.length(); i++) {		//	or an operator
 			char c = expreString.charAt(i);
 			if (c == '+' || (!previousArgumentIsNullOrLeftBracket && c == '-')
 					|| c == '*' || c == '/' || c == '(' || c == ')' || c == 'E'
@@ -269,6 +284,7 @@ public class StringCalculator {
 		return (LinkedList<String>) tokenized;
 	}
 
+	// Parse character by character to decide whether it's a number or not
 	private static final boolean isNumeric(final String s) {
 		if (s == null || s.isEmpty())
 			return false;
@@ -278,7 +294,7 @@ public class StringCalculator {
 			if (x == 0 && (c == '-') && (s.length() > 1))
 				continue; // negative
 			if (x > 0 && (c == '.') && !hasPoint) {
-				hasPoint = true;
+				hasPoint = true;	// A number can only has one decimal point at most
 				continue;
 			} // check points
 			if ((c >= '0') && (c <= '9'))
